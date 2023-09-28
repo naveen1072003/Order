89d3fa57cbeadcd11251440984cf63849ab1f90c
naveen1072003 <naveenrangaraju450@gmail.com>
2023-09-28 18:45:58 +0530

    image str

--- src/components/Home page/upload.jsx
+++ src/components/Home page/upload.jsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 
 function Imagesrc() {
   const [image, setImage] = useState({
-    file: null,
+    file: null
   });
 
-  const [dataUri, setRetrievedImage] = useState(null);
+  const [retrievedImage, setRetrievedImage] = useState(null);
 
   useEffect(() => {
     // Fetch the image from the backend when the component mounts
@@ -16,17 +16,8 @@ function Imagesrc() {
   const fetchImage = async () => {
     try {
       const response = await axios.get("http://localhost:8080/sample/getImage");
-      const { imageData } = response.data; // Assuming the response contains image data as a byte array
-
-      // Create a Blob from the byte array
-      const blob = new Blob([imageData], { type: "image/jpeg" }); // Specify the MIME type
-
-      // Create a data URI from the Blob
-      const dataUri = URL.createObjectURL(blob);
-
-      // Set the data URI in the state
-      setRetrievedImage(dataUri);
-      console.log(dataUri);
+      // console.log(response.data);
+      setRetrievedImage(response.data);
     } catch (e) {
       console.log(e);
     }
@@ -36,18 +27,13 @@ function Imagesrc() {
     try {
       const formData = new FormData();
       formData.append("file", image.file);
-
-      const response = await axios.post(
-        "http://localhost:8080/sample/uploadImage",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      console.log(response.data);
+      // formData.append("name", image.name); // Include the name field
+  
+      await axios.post("http://localhost:8080/sample/uploadImage", formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
     } catch (e) {
       console.log(e);
     }
@@ -55,15 +41,19 @@ function Imagesrc() {
 
   function Onchange(e) {
     setImage({
-      file: e.target.files[0],
+      file: e.target.files[0]
     });
   }
-  console.log(image);
 
+  console.log(image);
   return (
     <div className="">
-      <img src={dataUri} alt="logo" width={20} height={100} />
+
+      {/* <img src={retrievedImage} alt="logo" /><br/> */}
       <input type="file" name="file" id="" onChange={Onchange} />
+      <br />
+      <input type="text" name="name" id="" onChange={Onchange} />
+      <br />
       <button type="submit" onClick={apiCall}>
         Submit
       </button>
